782dfab3fc32f6635d7fcda987999cb88c20f85f
andrew-mekhail <andrew.mekhail96@gmail.com>
2023-08-12 10:07:25 -0700

    till task 7

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,20 @@
+/*here are the header */
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main(void)
+{
+  char alphabet[26] = "abcdefghijklmnopqrstuvwxyz" ;
+  int i ;
+  for ( i=25; i>=0; i--)
+    {
+      putchar (alphabet[i]);
+
+    }
+  putchar('\n');
+
+  return (0);
+}
+~                                                                                               
+~     

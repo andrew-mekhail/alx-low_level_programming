7e3008ee96d14da995dbf18e359194b2bd5ff25c
andrew-mekhail <andrew.mekhail96@gmail.com>
2023-08-11 18:37:03 -0700

    task 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+/*here are the header */
+#include <stdio.h>
+
+int main(void)
+{
+  char aplphabet [] = "abcdefghijklmnopqrstuvwxyz";
+  int i=0 ;
+  while (char[i] != '\n')
+    {
+      putcahr (alphabet[i]);
+      i++;
+    }
+  putchar('\n');
+
+
+  return (0);
+}

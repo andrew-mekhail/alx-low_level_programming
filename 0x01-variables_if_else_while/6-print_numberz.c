21dd92b06b27f6b99c3fa6f54a4722665df86c20
andrew-mekhail <andrew.mekhail96@gmail.com>
2023-08-12 09:59:13 -0700

    till task 6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+/*here are the header */
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main(void)
+{
+  int i ;
+  for ( i=0; i<9; i++)
+    {
+      putchar ( i + '0' );
+
+    }
+  putchar('\n');
+
+  return (0);
+}

0cb71ef810099c7be24bed0e17f7a7c4709e82eb
andrew-mekhail <andrew.mekhail96@gmail.com>
2023-08-12 10:45:17 -0700

    till task 9

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,24 @@
+/*here are the header */
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main(void)
+{
+  char alphabet[6] = "abcdef" ;
+  int i ;
+  for ( i=0; i<10; i++)
+    {
+      putchar (i+'0');
+
+    }
+  int j ;
+  for (j=0 ; j<6 ; j++)
+  {
+  
+  putchar(alphabet[j]);
+  }
+  putchar('\n');
+
+  return (0);
+}
---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,19 @@
+/*here are the header */
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main(void)
+{
+  int i ;
+  for ( i=0; i<10; i++)
+    {
+      putchar (i + '0');
+      putchar (',');
+      putchar (' ');
+
+    }
+  putchar('\n');
+
+  return (0);
+}

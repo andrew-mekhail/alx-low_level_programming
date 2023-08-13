b5b75278dfe4e71f0e669018a38643484a78e19c
andrew-mekhail <andrew.mekhail96@gmail.com>
2023-08-13 12:04:56 -0700

    till task 6

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main (void)
+{
+int a ;
+char b;
+long int c ;
+long long int d ;
+float e;
+
+printf("Size of a char: %d byte(s\n)",sizeof(b));
+printf("Size of an int: %d byte(s\n)",sizeof(a));
+printf("Size of a long int: %d byte(s\n)",sizeof(c));
+printf("Size of a long long int: %d byte(s\n)",sizeof(d));
+printf("Size of a float: %d byte(s\n)",sizeof(e));
+
+
+	
+return (0);
+}
+
+

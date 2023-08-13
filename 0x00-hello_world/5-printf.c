ec940393220facbbf4b7c2f6fb7356b5abe2bc5b
andrew-mekhail <andrew.mekhail96@gmail.com>
2023-08-13 15:05:58 -0700

    fixing betty

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
-
-int main (void)
+/**
+ * main - Entry point
+ * Description: 'using printf'
+ * Return: Always 0 (Success)
+ **/
+int main(void)
 {
 
 printf("with proper grammar, but the outcome is a piece of art,\n");

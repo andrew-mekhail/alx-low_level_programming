300c0a6c9cdf9c0ff35ed36da24a4ad959af65c6
andrew-mekhail <andrew.mekhail96@gmail.com>
2023-08-13 14:53:34 -0700

    betty again

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,8 @@
 /*header first here*/
 #include <stdio.h>
 #include <stdlib.h>
-/* the below function using puts to print */
-int main(void) 
+/*this function is to print using puts*/
+int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle\n");
 return (0);

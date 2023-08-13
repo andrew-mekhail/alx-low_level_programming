d2920a71a120aa56223c23cb5d99f5c288469920
andrew-mekhail <andrew.mekhail96@gmail.com>
2023-08-13 14:34:54 -0700

    fixing betty

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,8 @@
-#include <stdio.h> 
+#include <stdio.h>
 #include <stdlib.h>
 
-int main (void )
+int main(void) /*this function is to print the belo string using puts instead of printf*/
 {
-puts ("\"Programming is like building a multilingual puzzle\n");
+puts("\"Programming is like building a multilingual puzzle\n");
 return (0);
 }

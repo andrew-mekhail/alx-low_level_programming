7e760db3c0c5c447b0d91e2ee376af69261e3fea
andrew-mekhail <andrew.mekhail96@gmail.com>
2023-08-13 15:12:47 -0700

    fixing betty for all

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,22 +1,24 @@
 #include <stdio.h>
 #include <stdlib.h>
-
-int main (void)
+/**
+ * main - Entry point
+ * Description: 'printing size of each type'
+ * Return: Always 0 (Success)
+ **/
+int main(void)
 {
-int a ;
+int a;
 char b;
-long int c ;
-long long int d ;
+long int c;
+long long int d;
 float e;
 
-printf("Size of a char: %d byte(s)\n",sizeof(b));
-printf("Size of an int: %d byte(s)\n",sizeof(a));
-printf("Size of a long int: %d byte(s)\n",sizeof(c));
-printf("Size of a long long int: %d byte(s)\n",sizeof(d));
-printf("Size of a float: %d byte(s)\n",sizeof(e));
+printf("Size of a char: %d byte(s)\n", sizeof(b));
+printf("Size of an int: %d byte(s)\n", sizeof(a));
+printf("Size of a long int: %d byte(s)\n", sizeof(c));
+printf("Size of a long long int: %d byte(s)\n", sizeof(d));
+printf("Size of a float: %d byte(s)\n", sizeof(e));
 
-
-	
 return (0);
 }
 

b826ec381e88158e5df7c95c0e629a0dfa7cbb97
andrew-mekhail <andrew.mekhail96@gmail.com>
2023-08-14 13:25:38 -0700

    modifiying betty for last two

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,30 +1,30 @@
 #include <stdio.h>
 #include <stdlib.h>
+/**
+ * main - Entry point
+ * Description: 'prinitng another combination'
+ * Return: Always 0 (Success)
+ **/
 int main(void)
 {
-  int i ,j,m;
-  for ( i=0; i<10; i++)
-    {
-
-      for ( j=i+1; j<10 ; j++)
-      {
-          for ( m=j+1 ; m < 10 ; m++ )
-          {
-
-            putchar (i + '0' );
-            putchar ( j + '0');
-            putchar (m + '0');
-            if ((i != 7) || (j !=8) || (m != 9) )
-            {
-                putchar (',');
-                putchar (' ');
-
-            }
-          }
-
-        }
-
-    }
-    putchar ('\n');
-  return (0);
+int i, j, m;
+for (i = 0; i < 10; i++)
+{
+for (j = i + 1; j < 10 ; j++)
+{
+for (m = j + 1 ; m < 10 ; m++)
+{
+putchar (i + '0');
+putchar (j + '0');
+putchar (m + '0');
+if ((i != 7) || (j != 8) || (m != 9))
+{
+putchar (',');
+putchar (' ');
+}
+}
+}
+}
+putchar ('\n');
+return (0);
 }

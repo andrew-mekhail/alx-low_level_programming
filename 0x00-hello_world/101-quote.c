c51f0f3fb80d7c3b49011390f29a2bbe8976aed4
andrew-mekhail <andrew.mekhail96@gmail.com>
2023-08-14 16:43:09 -0700

    last try for task 8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -8,18 +8,29 @@
 int main(void)
 {
 int i;
-char output[57] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
-for (i = 0 ; i <= 57 ; i++)
-{
-if (output[i] == ',')
-{
-putchar(' ');
-i++;
-}
-else
+char output[32] = "and that piece of art is useful\"";
+char output2[16] = " - Dora Korpar, ";
+char output3[10] = "2015-10-19";
+for (i = 0 ; i < 32 ; i++)
 {
 putchar(output[i]);
 }
+for (i = 0 ; i < 16; i++)
+{
+putchar(output2[i]);
 }
+for (i = 0 ; i < 10 ; i++)
+{
+if (output3[i] == '-')
+{
+putchar(output3[i]);
+}
+else
+{
+putchar(output3[i] + '0');
+}
+
+}
+putchar('\n');
 return (1);
 }

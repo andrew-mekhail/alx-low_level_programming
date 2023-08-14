4aa2b3313f2abe2f0d77a5577a2faec2fb920c8e
andrew-mekhail <andrew.mekhail96@gmail.com>
2023-08-14 14:24:42 -0700

    last fix for task 1

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -24,7 +24,7 @@ printf("Last digit of %d is %d and is greater than 5\n", n, m);
 }
 else if (m < 6)
 {
-printf("Last digit of %d is %d and less than 6 and not 0\n", n, m);
+printf("Last digit of %d is %d and is less than 6 and not 0\n", n, m);
 }
 return (0);
 }

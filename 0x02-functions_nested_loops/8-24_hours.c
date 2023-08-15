db32d0ffc97697dac582969a656e28c20410d1cc
andrew-mekhail <andrew.mekhail96@gmail.com>
2023-08-15 11:44:56 -0700

    task 8

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,31 @@
+#include <stdlib.h>
+#include "main.h"
+/**
+ * jack_bauer -print every minute of the day
+ * void : takes no argument
+ * Return: nothing
+ **/
+void jack_bauer(void)
+{
+int i, j, k, m;
+for (i = 0 ; i < 2 ; i++)
+{
+for (j = 0; j < 4 ; i++)
+{
+for (k = 0; k < 6; k++)
+{
+for (m = 0; m < 10; m++)
+{
+_putchar(i + '0');
+_putchar(j + '0');
+_putchar(':');
+_putchar(k + '0');
+_putchar(m + '0');
+_putchar('\n');
+
+}	
+}
+}
+
+}
+}

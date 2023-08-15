ce86afe1858898938e3a3f941a48bef00cd9e90f
andrew-mekhail <andrew.mekhail96@gmail.com>
2023-08-15 10:02:12 -0700

    till task 7

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,12 @@
+#include <stdlib.h>
+#include "main.h"
+/**
+ * print_last_digit - finding the last digit of integer
+ * @c : takes one intger argument
+ * Return: the last digit of integer
+ **/
+int print_last_digit(int c)
+{
+int i = c % 10;
+return (i);
+}

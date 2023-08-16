6d128bb5eeb57ba55fedfdde67a65cf6e80d3bc9
andrew-mekhail <andrew.mekhail96@gmail.com>
2023-08-16 09:04:04 -0700

    task 13

---
+++ 0x02-functions_nested_loops/13-natural.c
@@ -0,0 +1,27 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * main - entry point
+ * description : sum all numbers that can be devided by 3 and 5
+ * Return: success
+ **/
+int main(void)
+{
+int i, sum = 0;
+for (i = 3; i < 1025 ; i++)
+{
+if ((i % 3 == 0) && (i % 5 == 0))
+{
+sum = sum + i;
+}
+else if ((i % 3 == 0) && (i % 5 != 0))
+{
+sum = sum + i;
+}
+else if ((i % 3 != 0) && (i % 5 == 0))
+{
+sum = sum + i;
+}
+}
+return (0);
+}

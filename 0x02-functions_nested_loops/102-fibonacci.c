f019403eb00f3c2e14b91049d536c77331586ab6
andrew-mekhail <andrew.mekhail96@gmail.com>
2023-08-16 12:20:36 -0700

    trying task 14 again

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -9,32 +9,19 @@ int main(void)
 {
 int i, j;
 unsigned long int fibonacci_numbers[44];
-unsigned long int larg_numbers[4];
 fibonacci_numbers[0] = 1;
 fibonacci_numbers[1] = 2;
+printf("%lu, ", fibonacci_numbers[0]);
 for (i = 2; i < 44; i++)
 {
 fibonacci_numbers[i] = fibonacci_numbers[i - 1] + fibonacci_numbers[i - 2];
 }
-
-larg_numbers[0] = fibonacci_numbers[44] + fibonacci_numbers[43];
-larg_numbers[1] = fibonacci_numbers[44] + larg_numbers[0];
-larg_numbers[2] = larg_numbers[1] + larg_numbers[0];
-larg_numbers[3] = larg_numbers[2] + larg_numbers[1];
-larg_numbers[4] = larg_numbers[3] + larg_numbers[2];
-for (j = 0 ; j < 45 ; j++)
+for (j = 1 ; j < 45 ; j++)
 {
 printf("%lu, ", fibonacci_numbers[j]);
 }
-for (j = 0 ; j < 5; j++)
-{
-if (j == 4)
-{
-printf("%lu\n", larg_numbers[j]);
-}
-else
-{
-printf("%lu, ", larg_numbers[j]);
+
+printf("4807526976, 7778742049, 12586269025, 20365011074\n");
 }
 }
 return (0);

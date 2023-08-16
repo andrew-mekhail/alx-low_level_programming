fc9343f4a6e8476e94d273af33a2f40472c78cd2
andrew-mekhail <andrew.mekhail96@gmail.com>
2023-08-16 12:25:27 -0700

    modifying task 14 loop

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -12,7 +12,7 @@ unsigned long int fibonacci_numbers[44];
 fibonacci_numbers[0] = 1;
 fibonacci_numbers[1] = 2;
 printf("%lu, ", fibonacci_numbers[0]);
-for (i = 2; i < 44; i++)
+for (i = 2; i < 45; i++)
 {
 fibonacci_numbers[i] = fibonacci_numbers[i - 1] + fibonacci_numbers[i - 2];
 }

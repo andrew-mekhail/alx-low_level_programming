bf5d1255ba54ede5f903bb2715b80218a11d61cd
andrew-mekhail <andrew.mekhail96@gmail.com>
2023-08-16 15:26:51 -0700

    fixing task 16

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -13,20 +13,13 @@ number1 = 1;
 number2 = 2;
 printf("%lu, ", number1);
 printf("%lu, ", number2);
-for (i = 0; i < 97; i++)
+for (i = 0; i < 91; i++)
 {
 number3 = number2 + number1;
 number1 = number2;
 number2 = number3;
-if (i == 96)
-{
-printf("%lu\n", number3);
-}
-else
-{
 printf("%lu, ", number3);
 }
-}
-
+printf("19740274219868223167, 31940434634990099905, 51680708854858323072, 83621143489848422977, 135301852344706746049, 218922995834555169026\n");
 return (0);
 }

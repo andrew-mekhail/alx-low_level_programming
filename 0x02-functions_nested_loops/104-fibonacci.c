967d70f3ec8ea2d2dda88e6ba27c3f2bc88b59c4
andrew-mekhail <andrew.mekhail96@gmail.com>
2023-08-16 14:52:31 -0700

    task 15

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * main - entry point
+ * description : printing fibonacci numbers
+ * Return: success
+ **/
+int main(void)
+{
+int i;
+unsigned long int number1, number2, number3;
+number1 = 1;
+number2 = 2;
+printf("%lu, ", number1);
+printf("%lu, ", number2);
+for (i = 0; i < 97; i++)
+{
+number3 = number2 + number1;
+number1 = number2;
+number2 = number3;
+printf("%lu, ", number3);
+}
+
+return (0);
+}

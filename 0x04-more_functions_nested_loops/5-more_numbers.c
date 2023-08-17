30470c4b209bf8d96066dd291c19fb7bec98ede8
andrew-mekhail <andrew.mekhail96@gmail.com>
2023-08-17 04:40:00 -0700

    till task 5

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,26 @@
+include <stdlib.h>
+#include "main.h"
+/**
+ * more_numbers - writes all numbers from 0 to 14 for 10 times
+ * void : takes no argument
+ * Return: nothing
+ */
+void more_numbers(void)
+{
+int i, j, k, m;
+for (j = 0 ' j < 10 ; j++)
+{	
+for (i = 0 ; i < 15 ; i++)
+{
+if (i > 9)
+{
+k = i / 10;
+_putchar(k + '0');
+}
+m = i % 10;
+_putchar(m + '0');
+
+}
+_putchar('\n');
+}
+}

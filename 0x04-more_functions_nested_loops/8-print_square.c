39959fddf39bbdb0f9fbb52769292bfcfa0dcdb1
andrew-mekhail <andrew.mekhail96@gmail.com>
2023-08-17 12:50:07 -0700

    task 8

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include "main.h"
+/**
+ * print_square -print square using #
+ * @size : takes one ineger argument
+ * Return: nothing
+ */
+void print_square(int size)
+{
+int i, j;
+if (size <= 0)
+{
+_putchar('\n');
+}
+else
+{
+for (i = 0 ; i < size ; i++)
+{
+for (j = 0; j < size ; j++)
+{
+_putchar('#');
+}
+_putchar('\n');
+}
+}
+}

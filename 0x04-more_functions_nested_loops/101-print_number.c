1d940082b2b6a2b54293d3c396356086d8ae609a
andrew-mekhail <andrew.mekhail96@gmail.com>
2023-08-18 10:00:57 -0700

    task 12

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,35 @@
+#include <stdlib.h>
+#include "main.h"
+/**
+ * print_number - print any integer number
+ * @n : takes one integer number
+ * Return: nothing
+ **/
+void print_number(int n)
+{
+int i, j, k, m, l, p;
+k = n;
+m = 0;
+p = 1;
+while (k != 0)
+{
+m++;
+k = k / 10;
+}
+for (i = 0; i < m ; i++)
+{
+p = p * 10;
+}
+for (i = 0; i < m ; i++)
+{
+if (n < 0)
+{
+_putchar('-');
+n = n * -1;
+}
+p = p / 10;
+j = n / p;
+l = j % 10;
+_putchar(l + '0');
+}
+}

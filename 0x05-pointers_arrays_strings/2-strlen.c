180c3b32fab1fdab94f4361290ca2abe1679608e
andrew-mekhail <andrew.mekhail96@gmail.com>
2023-08-21 15:17:08 -0700

    task 2

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include "main.h"
+/**
+ * _strlen - finding a length of string
+ * @s : poiner to string
+ * Return: nothing
+ */
+int _strlen(char *s)
+{
+int i = 0;
+while (*(s + i) != '\n')
+{
+i++;
+}
+return (i);
+}

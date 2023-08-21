8f8bcb4b5dd68d5e3cef1a0100f416050ef8b16b
andrew-mekhail <andrew.mekhail96@gmail.com>
2023-08-21 15:39:48 -0700

    till task 3

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include "main.h"
+/**
+ * _puts - printing a string
+ * @str : poiner to string
+ * Return: nothing
+ */
+void _puts(char *str)
+{
+int i = 0;
+while (*(str + i) != '\0')
+{
+_putchar(*(str + i) + '0');
+i++;
+}
+}

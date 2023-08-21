5359d4dc50b858bcd2fc34bc8b280b89ddcac534
andrew-mekhail <andrew.mekhail96@gmail.com>
2023-08-21 16:58:27 -0700

    task6

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include "main.h"
+/**
+ * puts2 - printing a string
+ * @str : poiner to string
+ * Return: nothing
+ */
+void puts2(char *str)
+{
+int i = 0;
+while (*(str + i) != '\0')
+{
+if (i % 2 == 0)
+{
+_putchar(*(str + i));
+i++;
+}
+else
+{
+i++;
+}
+}
+_putchar('\n');
+}

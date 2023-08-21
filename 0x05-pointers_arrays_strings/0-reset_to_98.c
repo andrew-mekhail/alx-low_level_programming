468630fb205466efdf2454e605f0f4163a495132
andrew-mekhail <andrew.mekhail96@gmail.com>
2023-08-21 14:33:25 -0700

    task0

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include <stdlib.h>
+#include "main.h"
+/**
+ * reset_to_98 - changain the valu of pointer to 9
+ * @n : poiner to integer
+ * Return: nothing
+ */
+void  reset_to_98(int *n)
+{
+*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+This projects are for Pointers and arrays and strings

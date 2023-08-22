37e3cff50c206096faa05c7c5f4b939fb06cbca6
andrew-mekhail <andrew.mekhail96@gmail.com>
2023-08-22 15:57:02 -0700

    task 100

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include "main.h"
+/**
+ * _atoi - printing a string of numbers
+ * @s : poiner to string
+ * Return: nothing
+ */
+int _atoi(char *s)
+{
+int i;
+int j = 0;
+if (*(s + 0) < 0)
+{
+_putchar('-');
+}
+while (*(s + i) != '\0')
+{
+if (*(s + i) > 57 || *(s + i) < 48)
+{
+break;
+}
+_putchar(*(s + i));
+i++;
+}
+return (0);
+}

08c207ca5151513f11ab5d104fdafa1173ea7fdf
andrew-mekhail <andrew.mekhail96@gmail.com>
2023-08-23 12:59:48 -0700

    till task 3

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,34 @@
+#include <stdlib.h>
+#include "main.h"
+/**
+ * _strcmp - comparing between two strings
+ * @s1 : pointer to string
+ * @s2 : pointer to string
+ * Return: integer 0 if equal -15 if frist oe is less than second one
+ *  15 if first one is bigger
+ */
+int *_strcmp(char *s1, char *s2)
+{
+int i = 0;
+int j = 0;
+while (*(s1 + i) != '\0')
+{
+i++;
+}
+while (*(s2 + j) != '\0')
+{
+j++;
+}
+if (i == j)
+{
+return (0);
+}
+else if (i > j)
+{
+return (15);
+}
+else if (i < j)
+{
+return (-15);
+}
+}

16329e2bbb0c02b8d15875676bfc06f5b51e86da
andrew-mekhail <andrew.mekhail96@gmail.com>
2023-08-24 16:34:48 -0700

    fixing task 3

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -12,11 +12,11 @@ int _strcmp(char *s1, char *s2)
 int i = 0;
 while (*(s1 + i) != '\0')
 {
-if (*(s1 + i) > *(s2 +i))
+if (*(s1 + i) > *(s2 + i))
 {
 return (15);
 }
-if (*(s1 + i) < *(s2 +i))
+if (*(s1 + i) < *(s2 + i))
 {
 return (-15);
 }

abe470fe413b4e1437a19d07c3b7e814f7068b0b
andrew-mekhail <andrew.mekhail96@gmail.com>
2023-08-24 16:33:00 -0700

    changing task 3

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -10,25 +10,17 @@
 int _strcmp(char *s1, char *s2)
 {
 int i = 0;
-int j = 0;
 while (*(s1 + i) != '\0')
 {
-i++;
-}
-while (*(s2 + j) != '\0')
-{
-j++;
-}
-if (i == j)
-{
-return (0);
-}
-else if (i > j)
+if (*(s1 + i) > *(s2 +i))
 {
 return (15);
 }
-else 
+if (*(s1 + i) < *(s2 +i))
 {
 return (-15);
 }
+i++;
+}
+return (0);
 }

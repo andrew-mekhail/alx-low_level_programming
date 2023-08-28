d241bb01f6c3f3b9e6af808cff242fdef937f11c
andrew-mekhail <andrew.mekhail96@gmail.com>
2023-08-28 16:43:47 -0700

    task1

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -9,11 +9,10 @@
  */
 char *_memset(char *s, char b, unsigned int n)
 {
-char *s = begin;
-while (n--)
+int i;
+for (i = 0 ; i < n; i++)
 {
-begin = b;
-begin;
+*(s + i) = b;
 }
-return (begin);
+return (s);
 }

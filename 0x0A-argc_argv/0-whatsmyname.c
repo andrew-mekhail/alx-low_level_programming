553d78ab37c7c13fbcfc58506112575f1434dc4f
andrew-mekhail <andrew.mekhail96@gmail.com>
2023-09-05 12:17:53 -0700

    trying task 0

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,12 +1,13 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include "main.h"
 /**
  * main - printinng the name of function
  * @argc : number of arguments
  * @argv : pointer to string
  * Return: success
  */
-void main (int argc, char *argv[])
+int main (int argc, char *argv[])
 {
 printf("%s\n", argv[0]);
 return (0);

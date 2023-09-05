8b669c5f7814be9e9a091531d4150d7660f751a6
andrew-mekhail <andrew.mekhail96@gmail.com>
2023-09-05 12:08:42 -0700

    new directory

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,28 @@
+include <stdlib.h>
+#include "main.h"
+/**
+ * print_diagonal - writes \ as times as the input for the function
+ * @n : takes one ineger argument
+ * Return: nothing
+ */
+void print_diagonal(int n)
+{
+int i, j;
+if (n <= 0)
+{
+_putchar('\n');
+}
+else
+{
+for (i = 0 ; i < n ; i++)
+{
+for (j = i; j > 0 ; j--)
+{
+_putchar(' ');
+}
+_putchar('\\');
+_putchar('\n');
+}
+}
+}
+~ 

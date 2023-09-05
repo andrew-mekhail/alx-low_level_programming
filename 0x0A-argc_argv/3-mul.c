1797010f83ce765f7ba428e4e87d2e1eafb5db42
andrew-mekhail <andrew.mekhail96@gmail.com>
2023-09-05 14:25:44 -0700

    till task 3

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "main.h"
+/**
+ * main - printinng the name of function
+ * @argc : number of arguments
+ * @argv : pointer to string
+ * Return: success
+ */
+int main(int argc, char *argv[])
+{
+int i, j, mul;
+if (argc < 2)
+{
+printf("Error\n");
+return (1);
+}
+else
+{
+i = atoi(argv[1]);
+j = atoi(argv[2]);
+mul = i *j;
+printf("%d\n", mul);
+return (0);
+}
+}

0649a08effe7e40c46edd44336f3f69ffc7cd9df
andrew-mekhail <andrew.mekhail96@gmail.com>
2023-09-05 13:42:07 -0700

    till task 2

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "main.h"
+/**
+ * main - printinng the name of function
+ * @argc : number of arguments
+ * @argv : pointer to string
+ * Return: success
+ */
+int main(int argc, char *argv[])
+{
+int i;
+if (argc > 0)
+{
+for (i = 1; i <= argc; i++)
+{	
+printf("%s\n", argv[i]);
+}
+}
+return (0);
+}

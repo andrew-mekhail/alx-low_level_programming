ca73bec78b670053ed32710b8be1df5d51fb7a1b
andrew-mekhail <andrew.mekhail96@gmail.com>
2023-09-05 12:03:45 -0700

    task0

---
+++ 0x0A-argc_argv/0-whatsmynameis.c
@@ -0,0 +1,13 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * main - printinng the name of function
+ * @argc : number of arguments
+ * @argv : pointer to string
+ * Return: success
+ */
+int main (int argc, char *argv[])
+{
+printf("%s\n", argv[0]);
+return (0);
+}
